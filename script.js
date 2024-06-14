400e9f8fdf9f9771cb0ceb8f8d8fe90d45cd643c
Fabrice <fabrice.ndagijimana@gmail.com>
2024-06-14 19:12:58 +0200

    implementation of a new function enabling users to get acces to weather of their current location simply by clicking the location icon

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const locationInput = document.getElementById("location-input");
 const searchButton = document.getElementById("search-button");
+const currentLocation = document.querySelector(".location-icon");
 const locationElement = document.getElementById("location");
 const temperature = document.getElementById("temperature");
 const description = document.getElementById("description");
@@ -13,9 +14,46 @@ searchButton.addEventListener("click", function () {
   }
 });
 
+currentLocation.addEventListener("click", function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        fetchWeatherDataByCoordinates(latitude, longitude);
+      },
+      function (error) {
+        console.error("Error getting location:", error);
+        alert("An error occurred while retrieving your location.");
+      }
+    );
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+});
+
 function fetchWeatherData(locationValue) {
-  const apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
+  const apiKey = "YOUR API KEY";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationValue}&units=metric&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.cod === 200) {
+        updateWeatherInfo(data);
+      } else {
+        alert("Location not found");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+      alert("An error occurred while fetching the weather data.");
+    });
+}
+
+function fetchWeatherDataByCoordinates(lat, lon) {
+  const apiKey = "YOUR API KEY"; // Replace with your OpenWeatherMap API key
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
   fetch(apiUrl)
     .then((response) => response.json())
